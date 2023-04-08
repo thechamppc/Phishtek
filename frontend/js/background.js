079a21efec3b80b50094af6f14a37edb0aa2aea2
Piyush Chaudhary <92359423+thechamppc@users.noreply.github.com>
2023-04-08 21:27:41 +0530

    Update background.js

--- frontend/js/background.js
+++ frontend/js/background.js
@@ -4,7 +4,7 @@ var isPhish = {};
 
 
 function fetchLive(callback) {
-  $.getJSON("https://raw.githubusercontent.com/sushant-max/phishing-detection-plugin/main/static/classifier.json", function(data) {
+  $.getJSON("https://raw.githubusercontent.com/thechamppc/Phishtek/main/static/classifier.json", function(data) {
       chrome.storage.local.set({cache: data, cacheTime: Date.now()}, function() {
           callback(data);
       });
